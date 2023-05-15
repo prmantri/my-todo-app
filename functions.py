b4e59b7ed1e7a9ba62b506bfa09a600a757d886e
Pravin <pravin.mantri@gmail.com>
2023-05-15 13:44:10 -0500

    Initial commit

---
+++ functions.py
@@ -0,0 +1,21 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """Read a text file and return the list of
+    to-do items
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_local, filepath=FILEPATH):
+    """Write a to do items list to the text file"""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_local)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
---
+++ web.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+# to run use streamlit run C:\PythonProject\web.py [ARGUMENTS]
+#  pip freeze > requirements.txt to store dependencies
+
+st.title(" My Todo App")
+st.subheader("This is subheader")
+st.write("This app is to increase your productivity")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="", placeholder="Add a new to..",
+              on_change=add_todo, key='new_todo')
+
+# st.session_state
+# # print("Hello")
